3a81eb677c68442c46fcc989dcc5baca98a91866
(Student) - Ethan Rennick <C00240102@ITCARLOW.IE>
2020-01-29 15:00:02 +0000

    I just tested the cell.h in sfml and im happy

--- LGame/Cell.h
+++ LGame/Cell.h
@@ -5,14 +5,15 @@ class Cell
 {
 	
 	private:
-	int typeOfCellData;
-		//0 could represent an empty cell, 1 a coin and 2 a tetro.
-	sf::Vector2f index;
-	//index is the cell's position in the array
+	int typeOfCellData;//0 could represent an empty cell, 1 a coin and 2 a tetro.
+	sf::Vector2f index;//index is the cell's position in the array
 
-	sf::RectangleShape box;
-	sf::Vector2f size;
-	sf::Vector2f position;
+
+
+
+	sf::RectangleShape box; //the tile we draw
+	sf::Vector2f size; //how big tile is
+	sf::Vector2f position; //the position on screen
 
 
 public:
--- LGameTest/test.cpp
+++ LGameTest/test.cpp
@@ -1,27 +1,77 @@
-#include "pch.h"
-
-
 #include "gtest/gtest.h"
+#include "..//LGame/Cell.h"
 
-#include "../LGame/Cell.h"
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
 
 TEST(TestCaseName, TestName) {
+	int levelData[4][4] = {
+		{1,2,2,0},
+		{0,2,3,0},
+		{0,2,3,0},
+		{0,3,3,1},
+	};
 
-	Cell newcell;
-	
-	newcell.setBoardPosition(3, 3);
-	newcell.setDataType(0);
-	newcell.setup();
+	Cell help[4][4];
 
+	for (int row = 0; row < 4; row++)
+	{
+		for (int col = 0; col < 4; col++)
+		{
+			//sets up the cell data and tells it to set itself up
+			help[row][col].setDataType(levelData[col][row]);
+			help[row][col].setBoardPosition(row, col);
+			help[row][col].setup();
+		}
+	}
 
+	for (int row = 0; row < 4; row++)
+	{
+		for (int col = 0; col < 4; col++)
+		{
+			EXPECT_EQ(help[row][col].getBox().getPosition().x, row * 100);
+			EXPECT_EQ(help[row][col].getBox().getPosition().y, col * 100);
+		}
+	}
 
-	EXPECT_EQ(newcell.getBox().getPosition().x, 100);
-	/*EXPECT_EQ(r.position.y, 100);
-	EXPECT_EQ(r.getrcolor(), sf::Color::Red);
-	EXPECT_EQ(r.position.x, 100);
-	EXPECT_EQ(r.position.x, 100);
-*/
+	EXPECT_EQ(help[0][0].typeOfCell(), 1);
+	EXPECT_EQ(help[0][1].typeOfCell(), 0);
+	EXPECT_EQ(help[0][2].typeOfCell(), 0);
+	EXPECT_EQ(help[0][3].typeOfCell(), 0);
 
-  EXPECT_TRUE(true);
+	EXPECT_EQ(help[1][0].typeOfCell(), 2);
+	EXPECT_EQ(help[1][1].typeOfCell(), 2);
+	EXPECT_EQ(help[1][2].typeOfCell(), 2);
+	EXPECT_EQ(help[1][3].typeOfCell(), 3);
+
+	EXPECT_EQ(help[2][0].typeOfCell(), 2);
+	EXPECT_EQ(help[2][1].typeOfCell(), 3);
+	EXPECT_EQ(help[2][2].typeOfCell(), 3);
+	EXPECT_EQ(help[2][3].typeOfCell(), 3);
+
+	EXPECT_EQ(help[3][0].typeOfCell(), 0);
+	EXPECT_EQ(help[3][1].typeOfCell(), 0);
+	EXPECT_EQ(help[3][2].typeOfCell(), 0);
+	EXPECT_EQ(help[3][3].typeOfCell(), 1);
+
+	EXPECT_EQ(help[0][0].getBox().getFillColor(), sf::Color::Yellow);
+	EXPECT_EQ(help[0][1].getBox().getFillColor(), sf::Color::White);
+	EXPECT_EQ(help[1][3].getBox().getFillColor(), sf::Color::Red);
+	EXPECT_EQ(help[2][0].getBox().getFillColor(), sf::Color::Blue);
+
+	//EXPECT_EQ(help[0][0].getBox().getPosition().x, 0);
+
+  //EXPECT_TRUE(true);
 }
 
